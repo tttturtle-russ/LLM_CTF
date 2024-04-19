52df96e9589756079538360e0c3321c6b26a01cb
TurtleRuss <tttturtleruss@gmail.com>
2024-04-19 16:04:16 +0800

    change start server shell --model param

--- func_calling/start_mistral_server.sh
+++ func_calling/start_mistral_server.sh
@@ -1,5 +1,5 @@
 python -u -m vllm.entrypoints.openai.api_server \
-    --model mistralai/Mistral-7B-Instruct-v0.2 \
+    --model /home/haoyang/Mistral-7B-Instruct-v0.2 \
     --host 0.0.0.0 \
     --max-model-len 29000
 
