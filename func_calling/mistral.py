7a096825ddbeeb051bd9b76bd99e1a910457c74d
TurtleRuss <tttturtleruss@gmail.com>
2024-04-26 23:11:26 +0800

    add test langchain

--- func_calling/mistral.py
+++ func_calling/mistral.py
@@ -58,6 +58,7 @@ class MistralAgent(BaseChatModel):
             messages=self._messages,
             model=self.model_name,
         )
+        print(resp)
         self._messages.append({"role": "assistant", "content": resp.choices[0].message.content})
         status.assistant_message(resp.choices[0].message.content)
         return ChatResult(
